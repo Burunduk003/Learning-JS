3e76cb5130926536972d18a2a5878f4212b70179
Burunduk <antoniok003@gmail.com>
2020-04-15 16:18:57 +0300

    первый коммит

---
+++ script.js
@@ -0,0 +1,39 @@
+'use strict';
+
+
+
+var nanber = 5;
+var string = "Hello!";
+var sym = Symbol();
+var boolean = true;
+null;
+undefined;
+var obj = {};
+
+console.log(4/0);
+console.log('string'*5);
+
+let something;
+console.log(something);
+
+let person = {
+    neme: 'Vasilisa',
+    age: 99,
+    isMarried: true
+}
+
+console.log(person['age']);
+
+let arr = ['spin.jpg', 'reel.phg', 'lure.bmp'];
+
+console.log(arr [0]);
+
+//alert('Hello World!');
+
+// let answer = confirm('are you a peace of shit?');
+
+// console.log(answer);
+
+let answer = prompt('ты дебил?', 'да');
+
+console.log(typeof(arr));
